ce4625d0942919a1d9a90806d3316cab8ed49787
永松 秀斗 <nagamatsu.hideto@b-architects.com>
2018-03-29 16:52:55 +0900

    AppContainerって何に使うんだ・・

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { AppContainer } from 'react-hot-loader'
 import { Router, Route, Switch } from 'react-router-dom';
 import createHashHistory from 'history/createHashHistory';
 const history = createHashHistory();
-import {  } from 'react-router-dom';
 
 // components
 import Hello from 'components/Hello';
@@ -15,13 +14,11 @@ if (module.hot) {
   module.hot.accept();
 }
 ReactDOM.render(
-  <AppContainer>
-    <Router history={history}>
-          <Switch>
-            <Route exact path="/" component={Hello} />
-            <Route exact path="/hello2" component={Hello2} />
-        </Switch>
-    </Router>
-  </AppContainer>,
+  <Router history={history}>
+        <Switch>
+          <Route exact path="/" component={Hello} />
+          <Route exact path="/hello2" component={Hello2} />
+      </Switch>
+  </Router>,
   document.body
 );
